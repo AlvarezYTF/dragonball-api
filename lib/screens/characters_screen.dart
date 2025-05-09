22995cf27d0c34aedb7a61a50721c6618179f435
mariocanola <macanola0@soy.sena.edu.co>
2025-05-09 09:39:27 -0500

    se realizo el cambio en la vista de los personajes, anexado a eso se le agrago el logo de dragon ball a la interfast y se simplificaron los detalles del personaje

--- lib/screens/characters_screen.dart
+++ lib/screens/characters_screen.dart
@@ -3,6 +3,8 @@ import '../models/character.dart';
 import '../services/api_service.dart';
 import '../widgets/character_card.dart';
 
+const String logoUrl = 'assets/logo_dragonballapi.webp';
+
 class CharactersScreen extends StatefulWidget {
   const CharactersScreen({super.key});
 
@@ -22,7 +24,24 @@ class _CharactersScreenState extends State<CharactersScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Personajes')),
+      appBar: AppBar(
+        backgroundColor: Colors.grey[300],
+        elevation: 0,
+        title: Row(
+          children: [
+            const Text('Personajes', style: TextStyle(color: Colors.black)),
+            const Spacer(), 
+            Image.network(
+              logoUrl,
+              height: 40,
+              fit: BoxFit.contain,
+              errorBuilder:
+                  (context, error, stackTrace) =>
+                      const Icon(Icons.error, color: Colors.red),
+            ),
+          ],
+        ),
+      ),
       body: SafeArea(
         child: FutureBuilder<List<Personaje>>(
           future: _personajes,
@@ -36,20 +55,27 @@ class _CharactersScreenState extends State<CharactersScreen> {
                 child: Text('No hay personajes disponibles.'),
               );
             }
-
             final personajes = snapshot.data!;
-            return GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 0.68,
-                crossAxisSpacing: 12,
-                mainAxisSpacing: 12,
-              ),
-              padding: const EdgeInsets.all(12),
-              itemCount: personajes.length,
-              itemBuilder: (context, index) {
-                return CharacterCard(personaje: personajes[index]);
-              },
+            return Column(
+              children: [
+                const SizedBox(height: 10),
+                Expanded(
+                  child: GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          childAspectRatio: 0.68,
+                          crossAxisSpacing: 12,
+                          mainAxisSpacing: 12,
+                        ),
+                    padding: const EdgeInsets.all(12),
+                    itemCount: personajes.length,
+                    itemBuilder: (context, index) {
+                      return CharacterCard(personaje: personajes[index]);
+                    },
+                  ),
+                ),
+              ],
             );
           },
         ),
--- lib/widgets/character_card.dart
+++ lib/widgets/character_card.dart
@@ -6,62 +6,42 @@ class CharacterCard extends StatelessWidget {
 
   const CharacterCard({super.key, required this.personaje});
 
-   @override
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: const Color(0xFF2C2C2C), // fondo oscuro
-        borderRadius: BorderRadius.circular(16),
+    return Card(
+      color: Colors.black.withAlpha(178),
+      elevation: 8,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
       ),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          // Imagen
           Expanded(
-            flex: 8,
-            child: Container(
-              padding: const EdgeInsets.all(10),
+            child: ClipRRect(
+              borderRadius: const BorderRadius.vertical(top: Radius.circular(20)),
               child: Image.network(
                 personaje.image,
-                fit: BoxFit.contain,
+                fit: BoxFit.cover,
               ),
             ),
           ),
-          // Contenido inferior
-          Container(
-            padding: const EdgeInsets.all(6),
-            decoration: const BoxDecoration(
-              color: Colors.black87,
-              borderRadius: BorderRadius.vertical(bottom: Radius.circular(16)),
-            ),
+          Padding(
+            padding: const EdgeInsets.all(10),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   personaje.name,
                   style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
-                ),
-                const SizedBox(height: 8),
-                Text('Base KI:', style: TextStyle(color: Colors.grey[400])),
-                Text(personaje.ki,
-                    style: const TextStyle(color: Colors.amber)),
-                const SizedBox(height: 4),
-                Text('Total KI:', style: TextStyle(color: Colors.grey[400])),
-                Text(personaje.maxKi,
-                    style: const TextStyle(color: Colors.amber)),
-                const SizedBox(height: 4),
-                Text(
-                  'Affiliation: ${personaje.affiliation}',
-                  style: const TextStyle(color: Colors.amber),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
               ],
             ),
-          )
+          ),
         ],
       ),
     );
